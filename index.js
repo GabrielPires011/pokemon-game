4c7016627cc94f1a69f54e05aa5fa2a20e699666
Gabriel Teixeira <gabrielpires011@hotmail.com>
2022-04-30 14:17:36 -0300

    Player Creation

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ context.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
 image.src = './img/Pellet Town.png'
-console.log(image)
+
+const playerImage = new Image()
+playerImage.src = './img/playerDown.png'
 
 image.onload = () => {
-    context.drawImage(image, -700, -500)  
+    context.drawImage(image, -735, -600)
+    context.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - playerImage.width / 4 / 2,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+    )
 }
