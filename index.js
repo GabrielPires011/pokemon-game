663c83123f926e866c4318ad82e3d8b366268f44
Gabriel Teixeira <gabrielpires011@hotmail.com>
2022-04-30 13:37:29 -0300

    Arranging map

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ canvas.height = 576
 
 context.fillStyle = 'white'
 context.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './img/Pellet Town.png'
+console.log(image)
+
+image.onload = () => {
+    context.drawImage(image, -700, -500)  
+}
