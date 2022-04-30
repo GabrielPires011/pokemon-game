bd287a411381cc20c3354e22e892d1f189444191
Gabriel Teixeira <gabrielpires011@hotmail.com>
2022-04-30 17:07:37 -0300

    Move Player Through Map on Keydown

--- index.js
+++ index.js
@@ -13,8 +13,42 @@ image.src = './img/Pellet Town.png'
 const playerImage = new Image()
 playerImage.src = './img/playerDown.png'
 
-image.onload = () => {
-    context.drawImage(image, -735, -600)
+class Sprite {
+    constructor({ position, velocity, image }) {
+        this.position = position
+        this.image = image
+    }
+    draw() {
+        context.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+
+const background = new Sprite({
+    position: {
+        x: -735,
+        y: -600
+    },
+    image: image
+})
+
+const Keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
     context.drawImage(
         playerImage,
         0,
@@ -26,4 +60,52 @@ image.onload = () => {
         playerImage.width / 4,
         playerImage.height
     )
+
+    if (Keys.w.pressed && lastKey === 'w') background.position.y += 3
+    else if (Keys.a.pressed && lastKey === 'a') background.position.x += 3
+    else if (Keys.s.pressed && lastKey === 's') background.position.y -= 3
+    else if (Keys.d.pressed && lastKey === 'd') background.position.x -= 3
 }
+
+animate()
+
+let lastKey = ''
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w':
+            Keys.w.pressed = true
+            lastKey = 'w'
+            break
+        case 'a':
+            Keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 's':
+            Keys.s.pressed = true
+            lastKey = 's'
+            break
+        case 'd':
+            Keys.d.pressed = true
+            lastKey = 'd'
+            break
+    }
+    console.log(Keys)
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            Keys.w.pressed = false
+            break
+        case 'a':
+            Keys.a.pressed = false
+            break
+        case 's':
+            Keys.s.pressed = false
+            break
+        case 'd':
+            Keys.d.pressed = false
+            break
+    }
+    console.log(Keys)
+})
